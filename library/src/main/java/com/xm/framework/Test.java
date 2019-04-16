b99b7fe95735b18e1d13f4dd39531704878986ad
叶晓明 <85349470@qq.com>
2019-04-16 17:02:39 +0800

    init

--- library/src/main/java/com/xm/framework/Test.java
+++ library/src/main/java/com/xm/framework/Test.java
@@ -1,7 +1,13 @@
 package com.xm.framework;
 
+import com.xm.framework.tools.secret.MD5;
+
 /**
  * Created by Mouse on 2019/4/1.
  */
 public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(MD5.md5("abcdd"));
+    }
 }
